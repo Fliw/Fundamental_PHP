d86b504bdc6f4bad187ec99dd996b9e0932b45b0
Fliw <fliw@digitalkode.com>
2019-11-02 20:53:01 +0700

    Menggabungkan antara variabel dengan string

---
+++ Variabel/CombineVar.php
@@ -0,0 +1,7 @@
+<?
+//disini saya membuat variabel bernama x yang saya isikan nama panggilan saya sendiri
+$x="Fliw";
+//kita akan mencoba menggabungkan antara kalimat biasa dengan isi dari variabel, perhatikan syntax dibawah ini
+echo "hai $x";
+//silahkan berimprovisasi
+?>
