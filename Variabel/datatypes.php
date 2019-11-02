56db153f739104d384fadfef60e248df243af6aa
Fliw <fliw@digitalkode.com>
2019-11-02 21:20:25 +0700

    berbagai tipe data dan contohnya

---
+++ Variabel/datatypes.php
@@ -0,0 +1,20 @@
+<?
+//php Mempunyai banyak data tipe, seperti:string,integer,float,boolean dan array
+//data tipe mempunyai peranan yg penting di php, setiap tipe data berbeda melakukan hal yg berbeda pula
+
+//disini kita membuat tipe data string, string bisa menampung banyak karakter huruf di php
+//string pun bisa single quotes ' maupun double quotes "
+$string="Hai Cogan";
+//kita membuat tipe data integer, integer menampung angka non desimal dari -2,147,483,648 dan 2,147,483,647.
+$integer=625;
+//tipe data float, float adalah tipe data yang sanggup menerima pecahan / desimal
+$float=535.355;
+//tipe data boolean, boolean hanya mempunyai 2 nilai, antara TRUE / FALSE, sama seperti saklar listrik, Nyala atau mati
+$boolean=TRUE;
+$boolean2=FALSE;
+//array adalah variabel yg punya banyak nilai, daripada buat 100 variabel dengan nama berbeda
+//lebih baik buat 1 variabel dengan 100 isi berbeda
+//semua perhitungan array dimulai dari 0, ingat... 0
+$buah=array('pisang','jambu','semangka');
+//selamat berimprovisasi !
+?>
