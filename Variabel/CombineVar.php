bc42cb72732ceb651f3323005a7a9db1743fbbca
Fliw <fliw@digitalkode.com>
2019-11-02 20:58:51 +0700

    Menggabungkan antara variabel dengan string dan berbagai operasi lainnya

--- Variabel/CombineVar.php
+++ Variabel/CombineVar.php
@@ -3,5 +3,9 @@
 $x="Fliw";
 //kita akan mencoba menggabungkan antara kalimat biasa dengan isi dari variabel, perhatikan syntax dibawah ini
 echo "hai $x";
+//selain itu juga, kita bisa menambah . (titik adalah penggabung antara string dengan variabel, maupun dengan string lagi)
+echo "hai".$x;
+//kita pun bisa menambah tag html didalam php (dibaca sebagai string)
+echo "<h1>Hai".$x."</h1>";
 //silahkan berimprovisasi
 ?>
