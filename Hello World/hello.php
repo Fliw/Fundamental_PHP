b46f2a7e0039632bd2ea7b6f9ca8bc94f9f3404c
Fliw <fliw@digitalkode.com>
2019-11-02 20:39:53 +0700

    Hello World

---
+++ Hello World/hello.php
@@ -0,0 +1,7 @@
+<?php
+//Semua coding PHP diawali Dengan <? atau <?php diakhiri dengan ? >
+//disini saya mengoutputkan tulisan Hello World, Untuk Output php kita menggunakan syntax "echo"
+//tulisan disini saya tulis dengan tanda petik agar php membacanya sebagai String ! 
+echo "Hello World ! Today I Will Learn PHP !";
+//kalian bisa berimprovisasi didalamnya 
+?>
