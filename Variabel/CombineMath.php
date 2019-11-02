6f886deffb5abee5f1d29e6d93edc3ed13b1d34e
Fliw <fliw@digitalkode.com>
2019-11-02 21:03:54 +0700

    operasi operasi terhadap variabel

---
+++ Variabel/CombineMath.php
@@ -0,0 +1,9 @@
+<?
+//kita buat 2 variabel, x dan y kita isi nilainya
+$x=3;
+$y=4;
+//kita output x dan y, hanya saja kita tambahkan operasi saat di output, yaitu operasi penambahan +
+//adapun banyak operasi lain seperti + - / * % 
+echo $x+$y;
+//silahkan berimprovisasi !
+?>
