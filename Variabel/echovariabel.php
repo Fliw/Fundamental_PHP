072f84921c1026a7a516f052cd740e8ee0afd64f
Fliw <fliw@digitalkode.com>
2019-11-02 20:49:43 +0700

    Output Nilai Variabel

---
+++ Variabel/echovariabel.php
@@ -0,0 +1,9 @@
+<?
+//disini Mendeklarasikan Sebuah Variabel Bernama x
+//semua deklarasi variabel diawali dengan tanda $
+//yang didalamnya saya isikan string "Hello World"
+$x="Hello World";
+//kemudian saya keluarkan isi dari variabel x tersebut menggunakan perintah echo
+echo $x;
+//silahkan berimprovisasi
+?>
