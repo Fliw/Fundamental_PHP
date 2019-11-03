9fdf953f8ceb88c67d3afa50864dcff08da1f8f8
Fliw <fliw@digitalkode.com>
2019-11-03 13:07:44 +0700

    Loop Dengan while

---
+++ Loop/whileLoop.php
@@ -0,0 +1,15 @@
+<?php
+//while adalah sebuah looping yang, selama kondisinya true, dia akan dijalankan sampe kondisinya false
+
+//kita deklarasi variabel bernama x dengan nilai 1
+$x=1;
+//ketika si x kurang atau setara dari 10 lakukan x++/x = x+1
+while($x<=10){
+     //kemudian kita echo di tiap perulangan
+    echo $x."<br>"; 
+    //operasi penambahan x agar si x bisa sampe 10, kan kalo kurang dari 10 ttp harus x++
+    $x++;
+   
+    
+}
+?>
